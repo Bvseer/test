9d1dec2d777adbed315cebc6efed33cd8d3d5371
Басир Хужамурадов <BXujamuradov@paymart.local>
2023-09-13 18:03:56 +0500

    in process

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,62 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
-	"log"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"net/http"
-	"regexp"
+	"test/internal/database"
+	"test/internal/models"
 )
 
 const host = "localhost"
 const port = "8080"
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	hi := "hello world"
+	users := []models.User{}
 	w.WriteHeader(http.StatusOK)
-	result, _ := json.Marshal(hi)
-	w.Write(result)
+	db := sqlx.DB{}
+	db.Select(&users, "SELECT * FROM users")
+
+	fmt.Println(users)
+}
+
+func init() {
+	database.ConnectToDB()
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/users", getUsers)
 	http.ListenAndServe(host+":"+port, r)
-}
-
-func doReq(url string) (content string) {
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-
-		log.Println(err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-
-		log.Println(err)
-		return
-	}
-
-	return string(body)
-}
-
-func getTitle(content string) (title string) {
-
-	re := regexp.MustCompile("<title>(.*)</title>")
-
-	parts := re.FindStringSubmatch(content)
-
-	if len(parts) > 0 {
-		return parts[1]
-	} else {
-		return "no title"
-	}
 }
